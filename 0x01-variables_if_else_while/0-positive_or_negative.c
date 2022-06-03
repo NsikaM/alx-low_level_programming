259d47128a8900443da97aa5361f875374fe0972
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 19:44:09 +0200

    DONE

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n>0)
-        {printf(n, "is positive")};
-    if (n<0)
-        {printf(n, "is negative")};
+        cout << n << "is positive";
+    else if (n<0)
+        cout << n << "is negative";
     else
-        {printf(n, "is zero")};
+        cout << n << "is zero";
 	return (0);
 }
