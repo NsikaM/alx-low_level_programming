167228213dd8964a9921cd3a725a26ad5072f0f4
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 22:26:05 +0200

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,8 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
-char *numbers = "0123456789";
-putchar(numbers);
+int num;
+
+for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+
+putchar('\n');
 return (0);
 }
