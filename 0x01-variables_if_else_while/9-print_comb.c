4cabc9a1299e0268c120f912667b8f12f84e6f13
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 22:30:38 +0200

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,17 @@
  */
 int main(void)
 {
-i=0
-while i<10
-{
-putchar(i,", ");
-i++;
-}
-return (0);
+int num;
+
+for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+
+putchar('\n');
 }
