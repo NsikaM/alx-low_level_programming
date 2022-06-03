e658f18b1f4111a723bdce2e1dd904453b60edfa
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 13:45:52 +0200

    First commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n%10
+    if m<6 and m!=0
+    printf("The last digit of",n,"is",m,"and is less than 6 and not 0")
+    if m=0
+    if m<6 and m!=0
+    printf("The last digit of",n,"is",m,"and is 0")
+    else
+    printf("The last digit of",n,"is",m,"and is greater than 5")
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+lower = "abcdefghijklmnopqrtsuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+putchar(lower);
+putchar(upper);
+return (0);
+}
