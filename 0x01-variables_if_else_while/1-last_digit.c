7f424dcd242118e2338e8d42d45dbc6c439c1322
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 21:03:41 +0200

    Commit 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,18 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n%10
-    if m<6 and m!=0
-{
-    printf("The last digit of",str[n],"is",str[m],"and is less than 6 and not 0")
-}
-    if m=0
-{
-    printf("The last digit of",str[n],"is",str[m],"and is 0");
-}
-    else
-{
-    printf("The last digit of",str[n],"is",str[m],"and is greater than 5")
-}
+	/* your code goes there */
+    m = n%10;
+    
+
+
 	return (0);
 }
+© 2022 GitHub, Inc.
