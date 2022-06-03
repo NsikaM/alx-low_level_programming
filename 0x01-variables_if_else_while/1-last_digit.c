abb2d7bfb9a053c8ca44bdfd33ab3a2322036247
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-03 21:25:43 +0200

    DNE

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,21 +20,21 @@ int main(void)
 if (n % 10 > 5)
 {
 puts("Last digit of ");
-printf("%i", n);
+puts("%i", n);
 puts(" and is greater than 5");
 }
 
 else if (n == 0)
 {
 puts("Last digit of ");
-printf("%i", n);
+puts("%i", n);
 puts(" and is 0");
 }
 
 else
 {
 puts("Last digit of ");
-printf("%i", n);
+puts("%i", n);
 puts(" and is less than 6 and not 0");
 }
 	return (0);
