1950cba94e083360cdb0f9882b3a4556cd5d1beb
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-07 20:41:11 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
-#include "main.c"
-
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
+#include <main.h>
 void print_alphabet(void)
 {
 	int n;
