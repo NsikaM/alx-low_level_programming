75b84bf8bee7d1118365e625b1f1cd32b3720832
Nsika Mkhwanazi <npmkhwanazi22@gmail.com>
2022-06-07 21:32:30 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 
 
 /**
- * 4-isalpha - a function that checks for alphabetic character
- * c: single letter input
+ * _isalpha - a function that checks for alphabetic character
+ * @c - single letter input
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
 int _isalpha(int c)
